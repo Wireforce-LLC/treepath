ade2fec942f62dc92c1a5b823b037fb81eaa9889
Maslow Vladislav <70482199+masloffvs@users.noreply.github.com>
2024-04-21 02:38:52 +0300

    add proxy_pass_request_headers

--- watch_routes.sh
+++ watch_routes.sh
@@ -27,6 +27,7 @@ generate_config() {
                 proxy_set_header X-Real-IP \$remote_addr;
                 proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
                 proxy_set_header X-Forwarded-Proto \$scheme;
+                proxy_pass_request_headers on;
             }
         }" > /etc/nginx/conf.d/$domain.conf
 
