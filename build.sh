74eaeea369d37291fa7f201570a9baaf84c77832
Maslow Vladislav <70482199+masloffvs@users.noreply.github.com>
2024-04-21 01:48:44 +0300

    update build tools

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
+# Build the Docker image
 docker build -t treepath .
-docker run -d -p 80:80 --name treepath treepath
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Command to run the Docker container with volume mounting
+docker run -d -p 80:80 -v "$(pwd)/routes.trp:/etc/nginx/routes.trp" treepath
