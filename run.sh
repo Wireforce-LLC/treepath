498a2db61f41465e6a0537d004adf7c6237ca785
Maslow Vladislav <70482199+masloffvs@users.noreply.github.com>
2024-04-21 01:59:02 +0300

    support hot-relaod

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-# Command to run the Docker container with volume mounting
-docker run -d -p 80:80 -v "$(pwd)/routes.trp:/etc/nginx/routes.trp" treepath
+# Name of the Docker container
+CONTAINER_NAME="treepath"
 
+# Check if the container is already running
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    echo "Container $CONTAINER_NAME is already running. Stopping and removing it..."
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
+fi
+
+# Run the Docker container with volume mounting
+echo "Starting container $CONTAINER_NAME..."
+docker run -d -p 80:80 -v "$(pwd)/routes.trp:/etc/nginx/routes.trp" --name $CONTAINER_NAME treepath
