cbd9053b00d162acfbc668fe5e0dc67a237df865
Maslow Vladislav <70482199+masloffvs@users.noreply.github.com>
2024-05-23 13:57:18 +0300

    add more echos

--- run.sh
+++ run.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+echo "Starting the Treepath application configurator..."
+
 # Name of the Docker container
 CONTAINER_NAME="treepath"
 
 # Check if the container is already running
 if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
     echo "Container '$CONTAINER_NAME' is already running. Stopping and removing it..."
+
+    echo "Stopping container '$CONTAINER_NAME'..."
     docker stop $CONTAINER_NAME
+
+    echo "Removing container '$CONTAINER_NAME'..."
     docker rm $CONTAINER_NAME
 fi
 
@@ -19,3 +25,5 @@ docker run \
     --restart always \
     --hostname $CONTAINER_NAME \
     --name $CONTAINER_NAME treepath
+
+echo "Container '$CONTAINER_NAME' started."
